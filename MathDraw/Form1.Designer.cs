f693c4f3131625694d5b04fce8745a8d99c57af7
Andrey Lopukhov <andreyevenflow@gmail.com>
2016-09-13 01:29:20 +0300

    Updated binary and program icon

--- MathDraw/Form1.Designer.cs
+++ MathDraw/Form1.Designer.cs
@@ -23,20 +23,21 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.minRand = new System.Windows.Forms.TextBox();
-            this.maxRand = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.minNum = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.maxNum = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.formula = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.maxNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.minNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maxRand = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minRand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -75,18 +76,6 @@
             this.splitContainer1.SplitterDistance = 126;
             this.splitContainer1.TabIndex = 2;
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(537, 249);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.formula);
@@ -108,102 +97,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings: ";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label1.Location = new System.Drawing.Point(3, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "min rand";
-            // 
-            // minRand
-            // 
-            this.minRand.Dock = System.Windows.Forms.DockStyle.Top;
-            this.minRand.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.minRand.Location = new System.Drawing.Point(3, 35);
-            this.minRand.MaxLength = 3;
-            this.minRand.Name = "minRand";
-            this.minRand.Size = new System.Drawing.Size(120, 22);
-            this.minRand.TabIndex = 1;
-            this.minRand.Text = "1";
-            // 
-            // maxRand
-            // 
-            this.maxRand.Dock = System.Windows.Forms.DockStyle.Top;
-            this.maxRand.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.maxRand.Location = new System.Drawing.Point(3, 74);
-            this.maxRand.MaxLength = 3;
-            this.maxRand.Name = "maxRand";
-            this.maxRand.Size = new System.Drawing.Size(120, 22);
-            this.maxRand.TabIndex = 3;
-            this.maxRand.Text = "256";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label2.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label2.Location = new System.Drawing.Point(3, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "max rand";
-            // 
-            // minNum
-            // 
-            this.minNum.Dock = System.Windows.Forms.DockStyle.Top;
-            this.minNum.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.minNum.Location = new System.Drawing.Point(3, 113);
-            this.minNum.MaxLength = 3;
-            this.minNum.Name = "minNum";
-            this.minNum.Size = new System.Drawing.Size(120, 22);
-            this.minNum.TabIndex = 5;
-            this.minNum.Text = "1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label3.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label3.Location = new System.Drawing.Point(3, 96);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 17);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "min num";
-            // 
-            // maxNum
-            // 
-            this.maxNum.Dock = System.Windows.Forms.DockStyle.Top;
-            this.maxNum.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.maxNum.Location = new System.Drawing.Point(3, 152);
-            this.maxNum.MaxLength = 3;
-            this.maxNum.Name = "maxNum";
-            this.maxNum.Size = new System.Drawing.Size(120, 22);
-            this.maxNum.TabIndex = 7;
-            this.maxNum.Text = "40";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label4.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label4.Location = new System.Drawing.Point(3, 135);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "max num";
-            // 
             // formula
             // 
             this.formula.Dock = System.Windows.Forms.DockStyle.Top;
             this.formula.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.formula.Location = new System.Drawing.Point(3, 191);
+            this.formula.Location = new System.Drawing.Point(3, 161);
             this.formula.MaxLength = 40;
             this.formula.Name = "formula";
-            this.formula.Size = new System.Drawing.Size(120, 22);
+            this.formula.Size = new System.Drawing.Size(120, 19);
             this.formula.TabIndex = 9;
             this.formula.Text = "i * j";
             // 
@@ -212,20 +113,121 @@
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Top;
             this.label5.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label5.Location = new System.Drawing.Point(3, 174);
+            this.label5.Location = new System.Drawing.Point(3, 147);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.Size = new System.Drawing.Size(56, 14);
             this.label5.TabIndex = 8;
             this.label5.Text = "formula";
             // 
+            // maxNum
+            // 
+            this.maxNum.Dock = System.Windows.Forms.DockStyle.Top;
+            this.maxNum.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.maxNum.Location = new System.Drawing.Point(3, 128);
+            this.maxNum.MaxLength = 3;
+            this.maxNum.Name = "maxNum";
+            this.maxNum.Size = new System.Drawing.Size(120, 19);
+            this.maxNum.TabIndex = 7;
+            this.maxNum.Text = "40";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(3, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "max num";
+            // 
+            // minNum
+            // 
+            this.minNum.Dock = System.Windows.Forms.DockStyle.Top;
+            this.minNum.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.minNum.Location = new System.Drawing.Point(3, 95);
+            this.minNum.MaxLength = 3;
+            this.minNum.Name = "minNum";
+            this.minNum.Size = new System.Drawing.Size(120, 19);
+            this.minNum.TabIndex = 5;
+            this.minNum.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(3, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "min num";
+            // 
+            // maxRand
+            // 
+            this.maxRand.Dock = System.Windows.Forms.DockStyle.Top;
+            this.maxRand.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.maxRand.Location = new System.Drawing.Point(3, 62);
+            this.maxRand.MaxLength = 3;
+            this.maxRand.Name = "maxRand";
+            this.maxRand.Size = new System.Drawing.Size(120, 19);
+            this.maxRand.TabIndex = 3;
+            this.maxRand.Text = "256";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(3, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "max rand";
+            // 
+            // minRand
+            // 
+            this.minRand.Dock = System.Windows.Forms.DockStyle.Top;
+            this.minRand.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.minRand.Location = new System.Drawing.Point(3, 29);
+            this.minRand.MaxLength = 3;
+            this.minRand.Name = "minRand";
+            this.minRand.Size = new System.Drawing.Size(120, 19);
+            this.minRand.TabIndex = 1;
+            this.minRand.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "min rand";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(537, 249);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(667, 288);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Math Draw";
